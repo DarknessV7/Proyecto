49ecd6b2049ddc6babf54a967ff9ec1d316a2a0a
DarknessV7 <35636630+DarknessV7@users.noreply.github.com>
2018-02-03 14:43:33 -0600

    Add files via upload

---
+++ metodosdeentrada.py
@@ -0,0 +1,20 @@
+B= input ("ingrese el valor de B ")
+C= input ("ingrese el valor de C ")
+
+a= B+C
+print a
+
+
+z= input ("ingrese el valor de z")
+l= input ("ingrese el valor de l")
+
+r= z*z*l/2
+print r
+
+f= input ("ingrese el valor de f")
+e= input ("ingrese el valor de e")
+
+n=f*e
+P=(n*2)/2
+print n
+print P
